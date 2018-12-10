48e5c8df0111c6f79c54e72a981d93ef4c290761
Luiz Antônio <campos.antoonio@gmail.com>
2018-12-10 18:00:56 -0300

    adicionando novos métodos para a lista simples

--- Listas/lista_simplesmente_encadeada.py
+++ Listas/lista_simplesmente_encadeada.py
@@ -1,10 +1,20 @@
+import collections
+
 class LinkedList:
 
-    def __init__(self):
+    def __init__(self, seq=None):
         self.__head = None  # aponta para o primeiro elemento da lista
         self.__tail = None  # aponta para o último elemento da lista
         self.__length = 0
         self.__iterating = None  # auxiliar para iterar com lazy evaluation
+
+        if seq is not None and isinstance(seq, collections.Iterable):
+
+            for e in seq:
+                self.append(e)
+
+        elif seq is not None:
+            raise TypeError('the object is not iterable')
 
     class Node:  # classe interna para a criação de um novo Nó
 
@@ -46,53 +56,136 @@ class LinkedList:
 
         return aux
 
-    def __delitem__(self, key):
+    def __setitem__(self, key, value):
 
-        if key < 0:  # tratando índices negativos (del lista[-1]) -> apaga o último elemento
+        if key < 0:  # Tratando índices negativos
             key += len(self)
 
-        if key < 0 or key >= len(self):  # informando possíveis erros
+        if key < 0 or key >= len(self):
             raise IndexError('list assignment index out of range')
 
-
-        current = self.__head
-        previous = None
         i = 0
-        while i <= key:
+        current = self.__head
 
-            if current is None:
-                break
+        while current is not None:
 
             if i == key:
 
-                # removendo o primeiro elemento da lista
-                if previous is None:
+                current.content = value
+                break
 
-                    self.__head = current.next
-                    current.next = None
-
-                # removendo o último elemento da lista
-                elif current.next is None:
-
-                    self.__tail = previous
-                    previous.next = None
-
-                #removendo o elemento no meio da lista
-                else:
-
-                    previous.next = current.next
-                    current.next = None
-
-                self.__length -= 1  # removendo um elemento
-
-                return
-
-            previous = current
             current = current.next
 
             i += 1
 
+    def __getitem__(self, key):
+
+        if key < 0:  # Tratando índices negativos
+            key += len(self)
+
+        if key < 0 or key >= len(self):
+            raise IndexError('list assignment index out of range')
+
+        i = 0
+        current = self.__head
+
+        while current is not None:
+
+            if i == key:
+
+                return current.content
+
+            current = current.next
+
+            i += 1
+
+    def __delitem__(self, key):
+
+        if isinstance(key, slice):  # Apagando através do fatiamento
+
+            start, stop, step = self.init_values_slice(key)
+
+            i = start
+
+            while 0 <= i < stop:
+
+                if i >= len(self):
+                    break
+
+                del self[i]
+                i += step
+
+                # Decrementando para compensar o item que foi removido
+                i -= 1
+                stop -= 1
+
+        else:
+
+            if key < 0:  # tratando índices negativos (del lista[-1]) -> apaga o último elemento
+                key += len(self)
+
+            if key < 0 or key >= len(self):  # informando possíveis erros
+                raise IndexError('list assignment index out of range')
+
+
+            current = self.__head
+            previous = None
+            i = 0
+            while i <= key:
+
+                if current is None:
+                    break
+
+                if i == key:
+
+                    # removendo o primeiro elemento da lista
+                    if previous is None:
+
+                        self.__head = current.next
+                        current.next = None
+
+                    # removendo o último elemento da lista
+                    elif current.next is None:
+
+                        self.__tail = previous
+                        previous.next = None
+
+                    #removendo o elemento no meio da lista
+                    else:
+
+                        previous.next = current.next
+                        current.next = None
+
+                    self.__length -= 1  # removendo um elemento
+
+                    return
+
+                previous = current
+                current = current.next
+
+                i += 1
+
         self.__iterating = None
+
+    def init_values_slice(self, item):
+
+        start = 0 if item.start is None else item.start
+        stop = len(self) if item.stop is None else item.stop
+        step = 1 if item.step is None else item.step
+
+        if start < 0:
+            start += len(self)
+
+            if start < 0:
+                start = 0
+
+        if stop < 0:
+            stop += len(self)
+
+        if step == 0:
+            raise ValueError('o valor do step não pode ser zero')
+
+        return start, stop, step
 
     def pop(self, i=-1):  # removendo o último elemento
         self.__delitem__(i)
@@ -149,14 +242,15 @@ class LinkedList:
     def append(self, value):  # inserindo um elemento na última posição
         self.insert(len(self), value)
 
-
 lista = LinkedList()
-lista.append(4)
-lista.append(5)
-lista.append(7)
+lista.append(9)
+lista.append(9)
+lista.append(9)
+lista.append(9)
+lista.append(9)
+lista.append(9)
+
 print(lista)
 
-lista.pop()
-print(lista)
-del lista[0]
+del lista[0:2]
 print(lista)
