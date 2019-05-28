2363042bf589375c8080a94d9b1fb61d7166e6b4
Luiz Antônio <campos.antoonio@gmail.com>
2019-05-28 15:51:06 -0300

    Add files via upload

--- dupla.py
+++ dupla.py
@@ -4,14 +4,44 @@ class NO:
         self.proximo = None
         self.anterior = None
 
+    def __str__(self):
+        return str(self.chave)
+
 
 class ListaDupla:
-    def __init__(self):
+    def __init__(self, tipo=None):
         self.primeiro = None
         self.ultimo = None
         self.tamanho = 0
+        self.tipo = tipo
+
+    def __len__(self):
+        return self.tamanho
+
+    def __str__(self):
+        saida = '{'
+
+        atual = self.primeiro
+        while atual is not None:
+            saida += atual.__str__() + ','
+            atual = atual.proximo
+
+        saida = saida[:-1]
+        saida += '}'
+
+        return saida
+
+    def validar_tipo(self, valor):
+        if self.tipo is not None:
+            if type(valor) != self.tipo:
+                return False
+            return True
+        return True
 
     def inserir(self, indice, valor):
+
+        if not self.validar_tipo(valor):
+            raise TypeError('Tipo de valor não permitido para a lista')
 
         novoNo = NO(valor)
 
@@ -28,8 +58,8 @@ class ListaDupla:
             self.primeiro = novoNo
 
         # inserindo no fim
-        elif indice == self.tamanho - 1:
-            novoNo.proximo = self.ultimo
+        elif indice == self.tamanho:
+            novoNo.anterior = self.ultimo
             self.ultimo.proximo = novoNo
 
             self.ultimo = novoNo
@@ -44,9 +74,11 @@ class ListaDupla:
 
                 if i == indice - 1:
                     novoNo.proximo = atual.proximo
-                    novoNo = atual
-                    atual.proximo = novoNo
                     atual.proximo.anterior = novoNo
+
+                    atual.proximo = novoNo
+                    novoNo.anterior = atual
+                    break
 
                 atual = atual.proximo
                 i += 1
@@ -57,42 +89,69 @@ class ListaDupla:
         self.inserir(0, valor)
 
     def inserir_fim(self, valor):
-        self.inserir(self.tamanho - 1, valor)
+        self.inserir(self.tamanho, valor)
 
-    def remover(self, indice, valor):
+    def remover(self, indice):
 
         i = 0
         atual = self.primeiro
 
         while i <= indice:
 
-            if i == 0:  # removendo o primeiro elemento da lista
-                self.primeiro = atual
-                self.primeiro.anterior = None
-                atual.proximo = None
+            if i == indice:
 
-            elif i == self.tamanho - 1:  # removendo o último elemento da lista
-                self.ultimo = atual.anterior
-                self.ultimo.proximo = None
-                atual.anterior = None
+                if len(self) == 1:
+                    self.primeiro = None
+                    self.ultimo = None
 
-            else:
+                elif i == 0:  # removendo o primeiro elemento da lista
+                    self.primeiro = atual.proximo
+                    self.primeiro.anterior = None
+                    atual.proximo = None
 
-                atual.anterior.proximo = atual.proximo
-                atual.proximo.anterior = atual.anterior
+                elif i == self.tamanho - 1:  # removendo o último elemento da lista
+                    self.ultimo = atual.anterior
+                    self.ultimo.proximo = None
+                    atual.anterior = None
 
-                atual.proximo = None
-                atual.anterior = None
+                else:
 
+                    atual.anterior.proximo = atual.proximo
+                    atual.proximo.anterior = atual.anterior
+
+                    atual.proximo = None
+                    atual.anterior = None
+
+                self.tamanho -= 1
                 return
 
             i += 1
             atual = atual.proximo
 
+    def remover_fim(self):
+        self.remover(len(self) - 1)
 
-lista = ListaDupla()
-lista.inserir_inicio(4)
-lista.inserir_inicio(3)
+    def remover_inicio(self):
+        self.remover(0)
+
+    def __getitem__(self, indice):
+
+        i = 0
+        atual = self.primeiro
+        while atual is not None:
+
+            if i == indice:
+                return atual.chave
+
+            i += 1
+            atual = atual.proximo
+
+
+lista = ListaDupla(int)
+lista.inserir_fim(4)
+lista.inserir_fim(3)
+lista.inserir_fim(7)
 lista.inserir_inicio(8)
-lista.inserir_inicio(10)
+lista.inserir_inicio(9)
+lista.inserir_fim(8.7)
 print(lista)
